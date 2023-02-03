03e9385a1dbb125a9e5e1d2ef623e245b4bbd688
ziei <37557946+ziei@users.noreply.github.com>
2023-02-02 20:19:57 -0800

    Updated  array in Episode class

--- api-calls/app/Models/Episode.php
+++ api-calls/app/Models/Episode.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Episode extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'image',
+        'season',
+        'episode',
+        'show_number',
+        'summary'
+    ];
 }
