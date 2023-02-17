bd67c77d9a54fb7797d92f7ca411c42d213c9d43
ziei <37557946+ziei@users.noreply.github.com>
2023-02-16 21:22:50 -0800

    Saving temp data added to form

--- crud-app/resources/views/products/form.blade.php
+++ crud-app/resources/views/products/form.blade.php
@@ -1,10 +1,11 @@
+{{-- Shared fillable data form for Create/Update --}}
 <label class="form-label" for="name">Name</label>
-<input class="form-input" type="text" name="name" id="name" value="{{$product->name}}">
+<input class="form-input" type="text" name="name" id="name" value="{{old('name', $product->name)}}">
 <label class="form-label" for="price">Price</label>
-<input class="form-input" type="text" name="price" id="price" value="{{$product->price}}">
+<input class="form-input" type="text" name="price" id="price" value="{{old('price', $product->price)}}">
 <label class="form-label" for="description">Description</label>
-<input class="form-input" type="text" name="description" id="description" value="{{$product->description}}">
+<input class="form-input" type="text" name="description" id="description" value="{{old('description', $product->description)}}">
 <label class="form-label" for="item_number">Item Number</label>
-<input class="form-input" type="text" name="item_number" id="item_number" value="{{$product->item_number}}">
+<input class="form-input" type="text" name="item_number" id="item_number" value="{{old('item_number', $product->item_number)}}">
 <label class="form-label" for="image">Image URL</label>
-<input class="form-input" type="text" name="image" id="image" value="{{$product->image}}">
+<input class="form-input" type="text" name="image" id="image" value="{{old('image', $product->image)}}">
