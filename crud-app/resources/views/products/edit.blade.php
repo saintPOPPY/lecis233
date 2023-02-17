cfd1d2282e4a9fa92f1696c196379bd586868aaa
ziei <37557946+ziei@users.noreply.github.com>
2023-02-16 21:13:39 -0800

    Shared file set-up

--- crud-app/resources/views/products/edit.blade.php
+++ crud-app/resources/views/products/edit.blade.php
@@ -19,16 +19,7 @@
 
         {{-- Data-entry Fields --}}
         <div class="form-group">
-            <label class="form-label" for="name">Name</label>
-            <input class="form-input" type="text" name="name" id="name" value="{{$product->name}}">
-            <label class="form-label" for="price">Price</label>
-            <input class="form-input" type="text" name="price" id="price" value="{{$product->price}}">
-            <label class="form-label" for="description">Description</label>
-            <input class="form-input" type="text" name="description" id="description" value="{{$product->description}}">
-            <label class="form-label" for="item_number">Item Number</label>
-            <input class="form-input" type="text" name="item_number" id="item_number" value="{{$product->item_number}}">
-            <label class="form-label" for="image">Image URL</label>
-            <input class="form-input" type="text" name="image" id="image" value="{{$product->image}}">
+            @include('products.form')
         </div>
 
         {{-- Buttons to store data, or cancel and return to index --}}
---
+++ crud-app/resources/views/products/form.blade.php
@@ -0,0 +1,10 @@
+<label class="form-label" for="name">Name</label>
+<input class="form-input" type="text" name="name" id="name" value="{{$product->name}}">
+<label class="form-label" for="price">Price</label>
+<input class="form-input" type="text" name="price" id="price" value="{{$product->price}}">
+<label class="form-label" for="description">Description</label>
+<input class="form-input" type="text" name="description" id="description" value="{{$product->description}}">
+<label class="form-label" for="item_number">Item Number</label>
+<input class="form-input" type="text" name="item_number" id="item_number" value="{{$product->item_number}}">
+<label class="form-label" for="image">Image URL</label>
+<input class="form-input" type="text" name="image" id="image" value="{{$product->image}}">
