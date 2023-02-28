c8b664b843aa8eddda5566819093ceace104d443
ziei <37557946+ziei@users.noreply.github.com>
2023-02-28 00:40:26 -0800

    Added Review Seeder

--- crud-app/database/migrations/2023_02_16_005746_create_products.php
+++ crud-app/database/migrations/2023_02_16_005746_create_products.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('description');
             $table->integer('item_number');
             $table->string('image');
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
