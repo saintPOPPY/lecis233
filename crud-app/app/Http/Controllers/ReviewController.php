ec019ac4309bb5817173b7f111c47d211919feff
ziei <37557946+ziei@users.noreply.github.com>
2023-02-28 10:07:26 -0800

    Updated ReviewController

--- crud-app/app/Http/Controllers/ReviewController.php
+++ crud-app/app/Http/Controllers/ReviewController.php
@@ -2,31 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-use \App\Models\Review;
-
-class ProductController extends Controller
+class ReviewController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * 
-     * @return \Illuminate\Http\Response
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        // Assigning variables to sort by 'name' in ascending order in our sql call below
-        $sortBy = $request->query('sortBy') ?? 'name';
-        $direction = $request->query('direction') ?? 'asc';
-
-        // Eager loaded products
-        // $products = Product::with('products')->orderBy($sortBy, $direction);
-
-        // Non-eager loaded products
-        $reviews = Review::query()->orderBy($sortBy, $direction);
-        return response(view('reviews.index', ['reviews' => $reviews->paginate(10)]));
+        //
     }
 
     /**
@@ -34,46 +22,32 @@ class ProductController extends Controller
      */
     public function create(): Response
     {
-        $review = new Review;
-        return response(view('reviews.create', ['review' => $review]));
+        //
     }
 
     /**
      * Store a newly created resource in storage.
-     * 
-     * @param \Illuminate\Http\Request
-     * @return \Illuminate\Http\Response 
-     * 
      */
     public function store(Request $request): RedirectResponse
     {
-        Review::create($this->validateData($request));
-        return redirect()->route('products.index')->with('success', 'Review was created successfully');
+        Review::create($this->validatedData($request));
+        return redirect()->route('products.index', $request->product_id)->with('success', 'Review was created successfully');
     }
 
     /**
      * Display the specified resource.
-     * 
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     * 
      */
     public function show(string $id): Response
     {
-        $review = Review::findOrFail($id);
-        return response(view('reviews.show', ['review' => $review]));
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
-     * 
-     * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function edit(string $id): Response
     {
-        $review = Review::findOrFail($id);
-        return response(view('reviews.edit', ['review' => $review]));
+        //
     }
 
     /**
@@ -81,8 +55,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id): RedirectResponse
     {
-        Review::find($id)->update($this->validateData($request));
-        return redirect()->route('products.index')->with('success', 'Review was updated successfully');
+        //
     }
 
     /**
