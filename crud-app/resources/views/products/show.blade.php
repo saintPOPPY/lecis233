efc7c6fa789b33c184d7f6cf4a2a7ace08446edc
ziei <37557946+ziei@users.noreply.github.com>
2023-02-28 18:20:30 -0800

    Div containers, delete working

--- crud-app/resources/views/products/show.blade.php
+++ crud-app/resources/views/products/show.blade.php
@@ -40,8 +40,11 @@
     <form action="{{ route('reviews.store') }}" method="POST">
       @csrf
 
-      {{-- Input(Select): Rating --}}
+      {{-- Rating & Comment Input --}}
       <div class="form-group">
+        <label class="form-label" for="product_id" hidden>Product ID</label>
+        <input type="hidden" name="product_id" id="product_id" value="{{ $product->product_id }}">
+
         <label for="ratingSelect">Your Rating</label>
         <select class="form-control" id="ratingSelect" value="{{ $product->rating }}">
           <option selected>Rate this Product</option>
@@ -51,22 +54,17 @@
           <option>4</option>
           <option>5</option>
         </select>
-      </div>
-  
-      {{-- Input: Comment --}}
-      <div class="form-group">
+      
         <label for="commentsTextArea">Comments</label>
         <textarea class="form-control" id="commentsTextArea" rows="4" value="{{ $product->comment }}">Begin typing your review here...</textarea>
       </div>
 
       {{-- Add Review Button --}}
       <div class="form-group">
-        <label class="form-label" for="product_id" hidden>Product ID</label>
-        <input type="hidden" name="product_id" id="product_id" value="{{ $product->product_id }}">
         <button type="submit" class="btn btn-primary">Add Review</button>
       </div>
     </form>
-</div>
+  </div>
   
   {{-- Table of Reviews --}}
   <div class="container mt-3">
